f5d5761494efa32ef7028665df087d04610c6f0e
lrr-2890 <921090271@qq.com>
2021-07-23 20:50:51 +0800

    tools

--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
 from data_reader import DataReader
+import pickle as pkl
 
 def id2word(id_sentence,id2word_dict):
     sentene = [ id2word_dict[id] for id in id_sentence]
     return ','.join(sentene)
+
+def pkl_load(f_path):
+    with open(f_path,'rb') as f:
+        return pkl.load(f)
+def pkl_dump(data,f_path):
+    pkl.dump(data,open(f_path,'wb'))
