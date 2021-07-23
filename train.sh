e5797fed9fc79a9537c215a8145397fe7225ff1b
lrr-2890 <921090271@qq.com>
2021-07-23 20:51:35 +0800

    添加参数

--- train.sh
+++ train.sh
@@ -1,12 +1,14 @@
 python trainer.py \
-    --input_file ./data/zh_wiki_sample_lines_500.txt \
-    --vocab_file ./data/vocabulary.txt \
-    --line_batch_size 1 \ 
-    --sample_batch_size 128 \
+    --input_file /home/stu/Documents/dataset/zhwiki_2021714/wiki_final.txt \
+    --vocab_file ./data_dev_1.1/vocabulary.txt \
+    --line_batch_size 8 \
+    --sample_batch_size 256 \
     --learning_rate 1e-3 \
     --num_train_epochs 50 \
     --output_dir ./embedding/ \
-    --seed 12345
+    --img_data_file ./data_dev_1.1/char_img_mean.pkl \
+    --char2ix_file ./data_dev_1.1/char2ix.pkl \
+    --seed 12345 
 
 # line_batch_size * sample_batch_size
 # line_batch_size 一次读几个句子
