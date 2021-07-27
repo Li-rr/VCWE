d937f10bf108eb7f89f2e910bbd88420e76b1163
lrr-2890 <921090271@qq.com>
2021-07-27 15:52:32 +0800

    获取一定范围的随机数

--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
-from data_reader import DataReader
+# from data_reader import DataReader
 import pickle as pkl
 import os
+import random
+# 生成一定范围内指定数目的无重复随机数
+def random_without_same(mi, ma, num):
+	temp = list(range(mi, ma))
+	random.shuffle(temp)
+	return temp[0:num]
 def id2word(id_sentence,id2word_dict):
     sentene = [ id2word_dict[id] for id in id_sentence]
     return ','.join(sentene)
@@ -15,3 +21,8 @@ def pkl_dump(data,f_path):
 def ensure_dir(path):
     if not os.path.exists(path):
         os.makedirs(path)
+
+
+if __name__ == "__main__":
+    indices = random_without_same(0,9,5)
+    print(indices)
