15367e5049950751f7e21a2f7e9c4a397c0a538f
lrr <921090271@qq.com>
2021-08-01 14:07:26 +0800

    。

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 python trainer.py \
     --input_file ./data/zh_wiki_sample_lines_500.txt \
     --vocab_file ./data/vocabulary.txt \
-    --line_batch_size 1 \ 
+    --line_batch_size 1 \
     --sample_batch_size 128 \
     --learning_rate 1e-3 \
     --num_train_epochs 50 \
