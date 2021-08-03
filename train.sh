72759d7f02e994db85df8df1a5e96329331f763d
lrr <921090271@qq.com>
2021-08-03 16:20:41 +0800

    实验代号惠南

--- train.sh
+++ train.sh
@@ -1,16 +1,20 @@
+#!/bin/sh
+module load anaconda/2020.11
+module load cuda/10.2
+source activate py36_pt15
 python trainer.py \
-    --input_file /home/stu/Documents/dataset/zhwiki_2021714/wiki_final.txt \
+    --input_file ./wiki_final.txt \
     --vocab_file ./data_dev_1.1/vocabulary.txt \
-    --line_batch_size 16 \
+    --line_batch_size 64 \
     --sample_batch_size 128 \
-	--emb_dim 128 \
+	--emb_dim 100 \
     --learning_rate 1e-3 \
-    --num_train_epochs 80 \
+    --num_train_epochs 50 \
     --output_dir ./embedding/ \
     --img_data_file ./data_dev_1.1/char_img_mean.pkl \
     --char2ix_file ./data_dev_1.1/char2ix.pkl \
     --seed 12345 \
-    --exp_name $1
+    --exp_name 惠南2
 
 # line_batch_size * sample_batch_size
 # line_batch_size 一次读几个句子
