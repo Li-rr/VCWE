29d001d24a409edb7e4c9781f977655676417d80
lrr-2890 <921090271@qq.com>
2021-08-03 14:33:21 +0800

    实验代号：野生动物园

--- train.sh
+++ train.sh
@@ -1,10 +1,11 @@
 python trainer.py \
     --input_file /home/stu/Documents/dataset/zhwiki_2021714/wiki_final.txt \
     --vocab_file ./data_dev_1.1/vocabulary.txt \
-    --line_batch_size 28 \
+    --line_batch_size 16 \
     --sample_batch_size 128 \
+	--emb_dim 128 \
     --learning_rate 1e-3 \
-    --num_train_epochs 50 \
+    --num_train_epochs 80 \
     --output_dir ./embedding/ \
     --img_data_file ./data_dev_1.1/char_img_mean.pkl \
     --char2ix_file ./data_dev_1.1/char2ix.pkl \
