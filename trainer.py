f537deb58b038a5fc692d219114c7ae0ec3ee0a7
lrr-2890 <921090271@qq.com>
2021-08-17 16:51:07 +0800

    使用绝对路径

--- trainer.py
+++ trainer.py
@@ -1,10 +1,12 @@
+from functools import partial
+from ray.tune.session import report
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from tqdm import tqdm_notebook as tqdm
 import numpy as np
 import argparse
-import sys
+import sys,os
 from PIL import Image
 from scipy import misc
 from torchsummary import summary
@@ -12,99 +14,181 @@ from visualdl import LogWriter # 百度可视化工具
 import datetime
 
 from torch.optim import lr_scheduler
-from utils import id2word,pkl_load
+from utils import ensure_dir, id2word,pkl_load,trial_name_string
 from data_reader import DataReader, Word2vecDataset
 from model import VCWEModel
 from optimization import VCWEAdam
 from logginger import init_logger 
 import random
 
+# 超参数自动搜索
+from ray import tune
+from ray.tune import CLIReporter, trial
+from ray.tune.schedulers import ASHAScheduler,FIFOScheduler
+
+# 导入评估词向量的东西
+from evaluation.all_wordsim import computRho
+
+
 class Word2VecTrainer:
     def __init__(self, input_file, vocabulary_file, img_data_file, char2ix_file, output_dir, maxwordlength, emb_dimension, line_batch_size, sample_batch_size, 
                 neg_num, window_size, discard, epochs, initial_lr, seed,exp_name):
-                 
-        torch.manual_seed(seed)
-        random.seed(seed)
-        # self.img_data = np.load(img_data_file)
-        self.img_data = pkl_load(img_data_file)
-        self.data = DataReader(input_file, vocabulary_file, char2ix_file, maxwordlength, discard, seed)
-        # sample_batch_size，用于构建基本的数据集
-        dataset = Word2vecDataset(self.data, window_size, sample_batch_size, neg_num)
-        # line_batch_size是一次取几行数据，可以设为1
-        self.dataloader = DataLoader(dataset, batch_size=line_batch_size,
-                                     shuffle=True, num_workers=0, collate_fn=dataset.collate)
+
+        self.input_file = input_file
+        self.vocabulary_file = vocabulary_file
+        self.seed = seed
+        self.img_data_file = img_data_file
+
+        self.char2ix_file = char2ix_file
+        self.maxwordlength = maxwordlength
+        self.discard = discard
+
+        self.window_size = window_size
+        self.sample_batch_size = sample_batch_size
+        self.neg_num = neg_num
+        self.line_batch_size = line_batch_size
 
         self.output_dir = output_dir
-        self.emb_size = len(self.data.word2id)
-        self.char_size = len(self.data.char2id)+1       #5031
         self.emb_dimension = emb_dimension
-        self.line_batch_size = line_batch_size # 在DataLoader中使用
         self.epochs = epochs
         self.initial_lr = initial_lr
         self.exp_name = exp_name
-        self.loggger = init_logger("myVcwe_{}".format(self.exp_name),"./logs")
-        self.VCWE_model = VCWEModel(
-            self.emb_size, 
-            self.emb_dimension, 
-            self.data.wordid2charid, 
-            self.char_size,
-            self.data.noise_dist,
-            self.loggger,
-            self.exp_name
+
+
+
+    def rho_acc(self,embds,word2id):
+        word_vecs = {}
+
+        for wid,w in word2id.items():
+            word_vecs[w] = embds[wid]
+
+        _,res_list = computRho(word_vecs,'/home/stu/LRR/trainW2C/VCWE/evaluation/word-sim/')
+
+        # print(res_list)
+        all_rho = sum(res_list)
+        # print(all_rho)
+        return all_rho,res_list
+
+    def train_tune(self,num_samples):
+      
+        # 配置超参数搜索空间
+        hyper_para_conf = {
+            # 'lr': tune.loguniform(1e-4, 1e-1),
+            'seed': tune.randint(1000,9000),
+            'lr': tune.grid_search([1e-3,2e-3,5e-4]),
+            'emb_dim': tune.grid_search([100,128]) # 100，128作为格点
+        }
+        # # 配置调度器
+        # scheduler = ASHAScheduler(
+        #     metric="rho",
+        #     mode="max",
+        #     max_t=self.epochs,
+        #     grace_period=1,
+        #     reduction_factor=2
+        # )
+        # scheduler = FIFOScheduler(
+            
+        # )
+        # 在命令行打印实验报告
+        report = CLIReporter(
+            metric_columns=['rho','loss','training_iteration','ch240','ch297','mc30','rg65']
+        )
+        result = tune.run(
+            self.train,
+            name = self.exp_name, # 实验名称
+            local_dir= "./ray_results",
+            trial_name_creator= tune.function(partial(trial_name_string,trail_name=self.exp_name,mode="name")),
+            trial_dirname_creator  = tune.function(partial(trial_name_string,trail_name=self.exp_name,mode="dir")),
+            # 指定训练资源
+            resources_per_trial={"cpu": 2, "gpu": 1},
+            config=hyper_para_conf, # 超参数空间
+            num_samples=num_samples, # 可以看作实验次数
+            # scheduler=scheduler, # 调度器
+            progress_reporter=report, # 进度报告器
+
         )
 
+    def train(self,config):
+        '''
+        在这里改造训练部分的代码
+        '''
+        torch.manual_seed(config['seed'])
+        random.seed(config['seed'])
+        # self.img_data = np.load(img_data_file)
+        self.img_data = pkl_load(self.img_data_file)
+        self.data = DataReader(self.input_file, self.vocabulary_file, self.char2ix_file, self.maxwordlength, self.discard, self.seed)
+        # sample_batch_size，用于构建基本的数据集
+        dataset = Word2vecDataset(self.data, self.window_size, self.sample_batch_size, self.neg_num)
+        # line_batch_size是一次取几行数据，可以设为1
+        self.dataloader = DataLoader(dataset, batch_size=self.line_batch_size,
+                                     shuffle=True, num_workers=0, collate_fn=dataset.collate)
+
+        self.output_dir = self.output_dir
+        self.emb_size = len(self.data.word2id)
+        self.char_size = len(self.data.char2id)+1       #5031
 
         
+
         
-        print("#---------------------------------------")
-        print(self.VCWE_model)
+
+
         self.use_cuda = True #torch.cuda.is_available()
-        
-        
         self.device = torch.device("cuda" if self.use_cuda else "cpu")
-        self.num_train_steps= int(len(self.dataloader) * self.epochs)
-        if self.use_cuda:
-            self.VCWE_model.cuda()
 
-
-    def train(self):
-        # img ([5031, 1, 40, 40]) 第一个维度代表不同的字
+        # 读取图片数据 img ([5031, 1, 40, 40]) 第一个维度代表不同的字
         self.img_data = torch.from_numpy(self.img_data).to(self.device)
 
         if self.img_data.dim() == 3:
             print(self.img_data.shape)
             self.img_data = self.img_data.unsqueeze(1)
             print(self.img_data.shape)
-        # sys.exit(0)
-        # print(self.img_data[0][0].shape)
-        # print("----")
-
-        # print(self.img_data[5].sum())
-        # temp2 = self.img_data[0][0].cpu().numpy() * 255
-        # # misc.imsave('out.jpg', temp2)        
-        # im = Image.fromarray(temp2)
         
-        # im = im.convert('L') 
+        
 
-        # im.save('outfile2.png')
-        # print(self.img_data.shape)
-        cur_time = datetime.datetime.now()
-        cur_time = datetime.datetime.strftime(cur_time,'%Y-%m-%d_%H:%M:%S')
-        logs_path = "./logs/" + self.exp_name +cur_time
+        self.num_train_steps= int(len(self.dataloader) * self.epochs)
+
+        print("--------------------------------初始化完成")
+
+        #----------------------------------------------------------------------
+        trial_name = tune.get_trial_name() # 实验代号
+        exp_dir = os.path.join("/home/stu/LRR/trainW2C/VCWE/logs/",self.exp_name) # 当前实验的文件夹
+        ensure_dir(exp_dir)
+        logs_path = os.path.join(exp_dir,trial_name)
+        # 日志文件的格式为: ./logs/新场/新场_001
+        # logs_path = "./logs/" + self.exp_name +cur_time        
+
+
+        emb_dir = "/home/stu/LRR/trainW2C/VCWE/embedding/{}/{}/".format(self.exp_name,trial_name)
+        ensure_dir(emb_dir)
         writer = LogWriter(logdir=logs_path)
-
+        self.loggger = init_logger("myVcwe_{}".format(self.exp_name),logs_path)
+        #---------------------------------------------
+        VCWE_model = VCWEModel(
+            self.emb_size,
+            config['emb_dim'],
+            self.data.wordid2charid, 
+            self.char_size,
+            self.data.noise_dist,
+            self.loggger,
+            self.exp_name
+        )
+        if self.use_cuda:
+            VCWE_model.cuda()
+        print(VCWE_model)
         no_decay = ['bias']
         optimizer_parameters = [
-             {'params': [p for n, p in self.VCWE_model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay_rate': 0.01},
-             {'params': [p for n, p in self.VCWE_model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay_rate': 0.0}
+             {'params': [p for n, p in VCWE_model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay_rate': 0.01},
+             {'params': [p for n, p in VCWE_model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay_rate': 0.0}
              ]
 		
+        print("lr:",config["lr"])
+        print("seed:",config["seed"])
         print("num_train_steps=",self.num_train_steps)
         optimizer = VCWEAdam(optimizer_parameters,
-                             lr=self.initial_lr,
+                             lr=config['lr'],
                              warmup=0.1,
                              t_total=self.num_train_steps)
-        scheduler = lr_scheduler.CosineAnnealingLR(optimizer, self.epochs, eta_min=1e-4, last_epoch=-1)
+        # scheduler = lr_scheduler.CosineAnnealingLR(optimizer, self.epochs, eta_min=1e-4, last_epoch=-1)
         steps = 0
         accumulation_steps = 4 # 用于梯度积累
         for epoch in range(self.epochs):
@@ -144,7 +228,7 @@ class Word2VecTrainer:
                     # print(pos_v)
                     # print(sentence)
                     # optimizer.zero_grad()
-                    loss = self.VCWE_model.forward(pos_u, pos_v, neg_v, self.img_data)
+                    loss = VCWE_model.forward(pos_u, pos_v, neg_v, self.img_data)
                     # print("loss's type",type(loss),loss)
                     running_loss += loss.item()
                     loss_2 = loss.item()
@@ -156,10 +240,10 @@ class Word2VecTrainer:
 
                     if((i+1)%accumulation_steps)==0:
                         optimizer.step() # update parameters of net
-                        scheduler.step() # 更新学习率
-                        writer.add_scalar(tag='lr',step=steps,value=scheduler.get_lr()[0])
+                        # scheduler.step() # 更新学习率
+                        # writer.add_scalar(tag='lr',step=steps,value=scheduler.get_lr()[0])
                         optimizer.zero_grad() # reset gradient
-                    if steps % 50 == 0:
+                    if steps % 200 == 0:
                         loss_num = loss.item() * accumulation_steps
                         writer.add_scalar(tag="loss",step=steps,value=loss_num)
                         self.loggger.info("steps:{}/{}, epochs: {}/{}, loss: {}".format(
@@ -168,13 +252,14 @@ class Word2VecTrainer:
                     elif steps % 500 == 0:
                         self.loggger.info("添加直方图，当前steps：{}".format(steps))
                         writer.add_histogram(
-                            tag="u's embed",values=self.VCWE_model.u_embeddings.cpu().data.numpy(),
+                            tag="u's embed",values=VCWE_model.u_embeddings.weight.cpu().data.numpy(),
                             step=steps,buckets=10
                         )
                         writer.add_histogram(
-                            tag="v's embed",values=self.VCWE_model.v_embeddings.cpu().data.numpy(),
+                            tag="v's embed",values=VCWE_model.v_embeddings.weight.cpu().data.numpy(),
                             step=steps,buckets=10
                         )
+
                     steps += 1
                     epoch_steps += 1
 
@@ -185,15 +270,30 @@ class Word2VecTrainer:
                     #     running_loss=0.0
                 # print("要结束了哦")
                 # sys.exit(0)
+            # 在这里进行评估rho
+            rho,rho_list = self.rho_acc(VCWE_model.u_embeddings.weight.cpu().data.numpy(),self.data.id2word)
+
             self.loggger.info("epoch: {}, avg_epoch_loss: {}".format(epoch+1,running_loss/epoch_steps))
             if (epoch+1) % 5 == 0 or (epoch+1) == self.epochs:
-                self.VCWE_model.save_embedding(self.data.id2word, self.output_dir+"zh_wiki_VCWE_ep"+str(epoch+1)+".txt")
-                state = {
-                    'model':self.VCWE_model.state_dict(),
-                    'optimizer':optimizer.state_dict(),
-                    'epoch': epoch
-                }
-                torch.save(state,"./model/{}_{}_vcwe_parame".format(self.exp_name,epoch+1))
+                VCWE_model.save_embedding(self.data.id2word, emb_dir+"zh_wiki_VCWE_ep"+str(epoch+1)+".txt")
+                # state = {
+                #     'model':VCWE_model.state_dict(),
+                #     'optimizer':optimizer.state_dict(),
+                #     'epoch': epoch
+                # }
+                # torch.save(state,"./model/{}_{}_vcwe_parame".format(self.exp_name,epoch+1))
+
+            # # TODO 保存检查点
+            # with tune.checkpoint_dir(epoch) as checkpoint_dir:
+            #     path = os.path.join(checkpoint_dir,"checkpoint")
+            #     torch.save((VCWE_model.state_dict(),optimizer),path)
+            # TODO 报告实验记录
+            tune.report(
+                rho=rho,loss=running_loss/epoch_steps,
+                ch240=rho_list[0],ch297=rho_list[1],mc30=rho_list[2],
+                rg65=rho_list[3]
+            )
+            
 
 
 def main():
@@ -284,7 +384,8 @@ def main():
                           exp_name=args.exp_name)
     # print("---?实验名称",args.exp_name)
     # sys.exit(0)
-    w2v.train()
+    # w2v.train()
+    w2v.train_tune(num_samples=4)
     
 if __name__ == "__main__":
     main()
